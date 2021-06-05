7168cee91639198a504d6cb8059b732334e42130
Resdan Logan <rezn5447@gmail.com>
2021-06-05 12:18:52 -0700

    lifts

--- src/shared/colors.ts
+++ src/shared/colors.ts
@@ -1,3 +1,4 @@
 export const indigo = '#1B4965';
 export const maxBlue = '#62B6CB';
 export const textDark = '#0F262C';
+export const transparent = 'transparent';
--- src/shared/data.ts
+++ src/shared/data.ts
@@ -1,84 +1,142 @@
+export const LIFTS = {
+  BACK_SQUAT: 'BACK SQUAT',
+  DEADLIFT: 'DEADLIFT',
+  ROMANIAN_DEADLIFT: 'ROMANIAN DEADLIFT',
+  DB_WALKING_LUNGE: 'DB WALKING LUNGE',
+  BARBELL_HIP_THRUST: 'BARBELL HIP THRUST',
+  LEG_EXTENSION: 'LEG EXTENSION',
+  LYING_LEG_CURL: 'LYING LEG CURL',
+  MACHINE_STANDING_CALF_RAISE: 'MACHINE STANDING CALF RAISE',
+  SINGLE_LEG_LYING_LEG_CURL: 'SINGLE-LEG LYING LEG CURL',
+  SINGLE_LEG_LEG_EXTENSION: 'SINGLE-LEG LEG EXTENSION',
+  MACHINE_SEATED_HIP_ABDUCTION: 'MACHINE SEATED HIP ABDUCTION',
+  CRUNCH: 'CRUNCH',
+  PLANK: 'PLANK',
+  BARBELL_BENCH_PRESS: 'BARBELL BENCH PRESS',
+  DB_INCLINE_PRESS: 'DB INCLINE PRESS',
+  MILITARY_PRESS: 'MILITARY PRESS',
+  CABLE_FLY: 'CABLE FLY',
+  ASSISTED_DIP: 'ASSISTED DIP',
+  DUMBBELL_SKULL_CRRUSHER: 'DUMBBELL SKULL CRRUSHER',
+  SINGLE_ARM_ROPE_TRICEP_EXTENSION: 'SINGLE-ARM ROPE TRICEP EXTENSION',
+  DUMBBELL_LATERAL_RAISE: 'DUMBBELL LATERAL RAISE',
+  SUPINATED_PULLDOWN: 'SUPINATED PULLDOWN',
+  CABLE_SEATED_ROW: 'CABLE SEATED ROW',
+  CHEST_SUPPORTED_T_BAR_ROW: 'CHEST-SUPPORTED T-BAR ,ROW',
+  SEATED_FACE_PULL: 'SEATED FACE PULL',
+  CABLE_REVERSE_FLY: 'CABLE REVERSE FLY',
+  DUMBBELL_SUPINATED_CURL: 'DUMBBELL SUPINATED CURL',
+};
+
 export const videos = [
-  {name: 'BACK SQUAT',
-  video: 'https://www.youtube.com/watch?v=dW5-C1fsMjk'},
-  {name: 'DEADLIFT',
-  video: 'https://www.youtube.com/watch?v=fc4_hq7tjkU'},
-  {name: 'ROMANIAN DEADLIFT',
-  video: 'https://www.youtube.com/watch?v=SE-2Y-3a1pY'},
-  {name: 'DB WALKING LUNGE',
-  video:' https://www.youtube.com/watch?v=RjWiwq1wgFg&t=2s'},
-  {name: 'BARBELL HIP THRUST',
-  video: 'https://www.youtube.com/watch?v=RjWiwq1wgFg&t=2s'},
-  
-  {name: 'LEG EXTENSION', 
-  video: 'https://www.youtube.com/watch?v=YyvSfVjQeL0'},
-  
-  {name: 'LYING LEG CURL', 
-  video: 'https://www.youtube.com/watch?v=1Tq3QdYUuHs'},
-  
-  
-  {name: 'MACHINE STANDING CALF RAISE', 
-  video: 'https://www.youtube.com/watch?v=7AVIgDN72c0'},
-  
-  
-  {name: 'SINGLE-LEG LEG EXTENSION', 
-  video: 'https://www.youtube.com/watch?v=3VIpG6rtFAc'},
-  
-  
-  {name: 'SINGLE-LEG LYING LEG CURL', 
-  video: 'https://www.youtube.com/watch?v=lQSlJHtsnRA'},
-  
-  
-  name: 'MACHINE SEATED HIP ABDUCTION', 
-  video: 'https://www.youtube.com/watch?v=-seSnS4N8Xo'
-  
-  name: 'CRUNCH', 
-  video: 'https://www.youtube.com/watch?v=NGRKFMKhF8s'
-  
-  {name: 'PLANK', 
-  video: 'https://www.youtube.com/watch?v=kL_NJAkCQBg'},
+  {name: 'BACK SQUAT', video: 'https://www.youtube.com/watch?v=dW5-C1fsMjk'},
+  {name: 'DEADLIFT', video: 'https://www.youtube.com/watch?v=fc4_hq7tjkU'},
+  {
+    name: 'ROMANIAN DEADLIFT',
+    video: 'https://www.youtube.com/watch?v=SE-2Y-3a1pY',
+  },
 
-  
-  {name: 'BARBELL BENCH PRESS', 
-  video: 'https://www.youtube.com/watch?v=esQi683XR44'},
+  {
+    name: 'DB WALKING LUNGE',
+    video: ' https://www.youtube.com/watch?v=RjWiwq1wgFg&t=2s',
+  },
+  {
+    name: 'BARBELL HIP THRUST',
+    video: 'https://www.youtube.com/watch?v=RjWiwq1wgFg&t=2s',
+  },
 
-  {name: 'DB INCLINE PRESS',
-  video: 'https://www.youtube.com/watch?v=ggJycLjz01E'},
+  {name: 'LEG EXTENSION', video: 'https://www.youtube.com/watch?v=YyvSfVjQeL0'},
 
-  {name: 'MILITARY PRESS',
-  video: 'https://www.youtube.com/watch?v=CnBmiBqp-AI'},
+  {
+    name: 'LYING LEG CURL',
+    video: 'https://www.youtube.com/watch?v=1Tq3QdYUuHs',
+  },
 
-  {name: 'CABLE FLY',
-  video: 'https://www.youtube.com/watch?v=KJwiu8ttuZ0'},
+  {
+    name: 'MACHINE STANDING CALF RAISE',
+    video: 'https://www.youtube.com/watch?v=7AVIgDN72c0',
+  },
 
-  {name:' ASSISTED DIP',
-  video: 'https://www.youtube.com/watch?v=wjUmnZH528Y'},
+  {
+    name: 'SINGLE-LEG LEG EXTENSION',
+    video: 'https://www.youtube.com/watch?v=3VIpG6rtFAc',
+  },
 
-  {name: 'DUMBBELL SKULL CRUSHER',
-  video: 'https://www.youtube.com/watch?v=ZUZOn9c1VVI'},
+  {
+    name: 'SINGLE-LEG LYING LEG CURL',
+    video: 'https://www.youtube.com/watch?v=lQSlJHtsnRA',
+  },
 
-  {name: 'SINGLE-ARM ROPE TRICEP EXTENSION',
-  video: 'https://www.youtube.com/watch?v=FxK2mF0iPXs'},
+  {
+    name: 'MACHINE SEATED HIP ABDUCTION',
+    video: 'https://www.youtube.com/watch?v=-seSnS4N8Xo',
+  },
 
-  {name: 'DUMBBELL LATERAL RAISE',
-  video: 'https://www.youtube.com/watch?v=6m7JO28RqZg'},
+  {name: 'CRUNCH', video: 'https://www.youtube.com/watch?v=NGRKFMKhF8s'},
 
-  {name: 'SUPINATED PULLDOWN',
-  video: 'https://www.youtube.com/watch?v=apzFTbsm7HU'},
+  {name: 'PLANK', video: 'https://www.youtube.com/watch?v=kL_NJAkCQBg'},
 
-  {name: 'CABLE SEATED ROW',
-  video: 'https://www.youtube.com/watch?v=4mRy8U542Fo'},
+  {
+    name: 'BARBELL BENCH PRESS',
+    video: 'https://www.youtube.com/watch?v=esQi683XR44',
+  },
 
-  {name: 'CHEST-SUPPORTED T-BAR ROW',
-  video: 'https://www.youtube.com/watch?v=w0KnlQ-b7jw'},
+  {
+    name: 'DB INCLINE PRESS',
+    video: 'https://www.youtube.com/watch?v=ggJycLjz01E',
+  },
 
-  {name: 'SEATED FACE PULL',
-  video: 'https://www.youtube.com/watch?v=HSoHeSjvIdY'},
+  {
+    name: 'MILITARY PRESS',
+    video: 'https://www.youtube.com/watch?v=CnBmiBqp-AI',
+  },
 
-  {name: 'CABLE REVERSE FLY',
-  video: 'https://www.youtube.com/watch?v=QnXb1dh_RjQ'},
+  {name: 'CABLE FLY', video: 'https://www.youtube.com/watch?v=KJwiu8ttuZ0'},
 
-  {name: 'DUMBBELL SUPINATED CURL',
-  video: 'https://www.youtube.com/watch?v=ykJmrZ5v0Oo'},
+  {name: ' ASSISTED DIP', video: 'https://www.youtube.com/watch?v=wjUmnZH528Y'},
 
-]
+  {
+    name: 'DUMBBELL SKULL CRUSHER',
+    video: 'https://www.youtube.com/watch?v=ZUZOn9c1VVI',
+  },
+
+  {
+    name: 'SINGLE-ARM ROPE TRICEP EXTENSION',
+    video: 'https://www.youtube.com/watch?v=FxK2mF0iPXs',
+  },
+
+  {
+    name: 'DUMBBELL LATERAL RAISE',
+    video: 'https://www.youtube.com/watch?v=6m7JO28RqZg',
+  },
+
+  {
+    name: 'SUPINATED PULLDOWN',
+    video: 'https://www.youtube.com/watch?v=apzFTbsm7HU',
+  },
+
+  {
+    name: 'CABLE SEATED ROW',
+    video: 'https://www.youtube.com/watch?v=4mRy8U542Fo',
+  },
+
+  {
+    name: 'CHEST-SUPPORTED T-BAR ROW',
+    video: 'https://www.youtube.com/watch?v=w0KnlQ-b7jw',
+  },
+
+  {
+    name: 'SEATED FACE PULL',
+    video: 'https://www.youtube.com/watch?v=HSoHeSjvIdY',
+  },
+
+  {
+    name: 'CABLE REVERSE FLY',
+    video: 'https://www.youtube.com/watch?v=QnXb1dh_RjQ',
+  },
+
+  {
+    name: 'DUMBBELL SUPINATED CURL',
+    video: 'https://www.youtube.com/watch?v=ykJmrZ5v0Oo',
+  },
+];
